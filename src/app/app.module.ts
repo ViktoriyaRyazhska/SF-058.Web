2d1b1067b27fccccc5d480f2b6b6946d8b81eda4
Mary Kozelska <maryswan811@gmail.com>
2022-08-30 15:11:57 +0300

    Left sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SidebarComponent} from "./left-sidebar/sidebar.component";
+import { LocalNavigationComponent } from './local-navigation/local-navigation.component';
+import { SocialNetworksComponent } from './social-networks/social-networks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LocalNavigationComponent,
+    SocialNetworksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
